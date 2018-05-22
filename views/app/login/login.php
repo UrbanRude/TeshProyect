8266e883c013e97e9a0e982e47b412f081cbefe2
Durruti17 <o.o_yeah@hotmail.com>
2018-05-21 23:16:50 -0500

    Finalizo estilo del login

--- views/app/login/login.php
+++ views/app/login/login.php
@@ -5,17 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
+    <div class="box">
+        <div class="logoTesh">
+            <img src="img/logoTesh.bmp">
+        </div>
     <form>
-    <h1>Huevos durruti</h1>
-    <label for="user">Usuario:</label>
-    <input type="text" id="user">
-    <label for="pwd">Contraseña</label>
-    <input type="password" id="pwd">
-    <button id="btnLogin">Ingresar</button>
+
+        <div class="inputBox">
+            <input type="text" name="" require="" id="user" >
+            <label for="user">Usuario:</label>
+            
+        </div>
+
+        <div class="inputBox">
+            <input type="password" name="" require="" id="pwd" >
+            <label for="pwd">Contraseña</label>
+            
+        </div>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"> Remember me
+                </label>
+            </div>
+
+                <div class="lock">
+                     <a href="">
+                        <img src="img/lock.svg"> Recover password?
+                     </a>
+                </div>
+
+    <button type="submit" id="btnLogin">Login</button>
+
     </form>
     <script src="./views/js/jquery.js"></script>   
     <script src="./views/app/login/login.js"></script>
+    </div>
 </body>
 </html>
